029340197e0ea1548680b55ed95a21e9cd2e7c48
Dulce Jaramillo <djaramillo@LABA.PSWAS.LOCAL>
2023-08-30 15:44:38 -0700

    finished bets

--- index.js
+++ index.js
@@ -75,14 +75,33 @@ let totalH = 0;
 let totalC = 0;
 let totalD = 0;
 let totalS = 0;
-let heartOdds = document.querySelector("heartOdds")
-let diamondOdds = document.querySelector("diamondOdds")
-let spadeOdds = document.querySelector("spadeOdds")
-let clubOdds = document.querySelector("clubOdds")
 
-console.log(arrayOfHearts);
+// placing bets
+let money= document.querySelector("#money")
+let dollars = 500
+let form= document.querySelector("form")
+let heartbet= document.querySelector("#heartbet")
+let clubbet= document.querySelector("#clubbet")
+let diamondbet= document.querySelector("#diamondbet")
+let spadebet= document.querySelector("#spadebet")
+let bet =document.getElementsByClassName("#bet")
+let heartsProb;
+let clubsProb;
+let diamondProb;
+let spadeProb
+let heartOdd
+let diamondOdd
+let spadeOdd
+let clubOdd
 let randomCard
 let sixCards = [card1, card2, card3, card4, card5, card6]
+let heartOdds = document.querySelector("#heartOdds")
+ let diamondOdds = document.querySelector("#diamondOdds")
+ let spadeOdds = document.querySelector("#spadeOdds")
+ let clubOdds = document.querySelector("#clubOdds")
+ let wager=0
+ let betOn;
+
 
 // function to make horses move
 const moveOneSpot= (suit)=>{
@@ -94,7 +113,7 @@ const moveOneSpot= (suit)=>{
             }else{
                 arrayOfHearts[i].hidden=true
             }
-        }
+           }
         
     }
 
@@ -105,8 +124,7 @@ if (suit == "c"){
             arrayOfClubs[i].hidden=false
         }else{
             arrayOfClubs[i].hidden=true
-        }
-    }
+        } }
     
 }
 
@@ -118,8 +136,7 @@ if (suit == "d"){
         }else{
             arrayOfDiamonds[i].hidden=true
         }
-    }
-    
+    }  
 }
 
 if (suit == "s"){
@@ -129,17 +146,11 @@ if (suit == "s"){
             arrayOfSpades[i].hidden=false
         }else{
             arrayOfSpades[i].hidden=true
-        }
+        } 
     }
-    
-}};
+}
 
-
-
-
-
-
-
+};
 
 // game start by click
 backOfCard.addEventListener('click',function(){
@@ -150,13 +161,17 @@ backOfCard.addEventListener('click',function(){
                     randomCard = cards[randomIndex];
                     sixCards[i].src = randomCard.img;
                     cutCards.push(randomCard.card)
+                    
                     cards.splice(randomIndex,1);
                     
                     }, i * 100); 
-                   
+               
                    }
-                   console.log(cutCards)
+                  
         isFirstClick = false
+
+
+
     } else{
         let randomIndex = Math.floor(Math.random()*cards.length);
         randomCard = cards[randomIndex];
@@ -165,69 +180,191 @@ backOfCard.addEventListener('click',function(){
         cardDeck.src = randomCard.img;
         if (suit === "h"){
           moveOneSpot("h")
+          
         }else if (suit=== "d"){
             moveOneSpot("d")
+            
         }else if (suit==="s"){
             moveOneSpot("s")
+           
         }else  if (suit==="c"){
             moveOneSpot("c")
+           
         }
         cards.splice(randomIndex,1);
     } 
+    if (currentDiamond>6){
+        alert("Diamonds win!")
+        if (betOn=="d"){
+            dollars+= wager*2
+            money.textContent= "$ " + dollars
+        }
+
+    }
+    
+    if (currentSpades>6){
+        alert("Spades Win!")
+        if (betOn=="s"){
+            dollars+= wager*2
+            money.textContent= "$ " + dollars
+        }
+        
+    }if (currentClub>6){
+        alert("clubs win!")
+        if (betOn=="c"){
+            dollars+= wager*2
+            money.textContent= "$ " + dollars
+        }
+    } if (currentHeart>6){
+        alert("hearts wins")
+        if (betOn=="h"){
+            dollars+= wager*2
+            money.textContent= "$ " + dollars
+        }
+    }  
+
+    // calculate odds
+// counts suit
+// figure odds
+// 
+    setTimeout(()=>{
+
     
     for (let i = 0; i < cutCards.length; i++) {
-        if( cutCards[1] =='h'){
-            totalH = totalH + 1
-            heartsProb= parseInt(totalH/6)
-            heartsOdd= parseInt(heartsProb-1)
+       
+        
+        if( cutCards[i][1] =='h'){
+            totalH += 1
+            heartsProb= parseInt((totalH/6)*10)
+            heartOdd= parseInt(heartsProb-1)
+            // heartOdd = (13 - totalH) / 46;
             
         }
         
-        if( cutCards[1] =='s'){
+        if( cutCards[i][1] =='s'){
             totalS = totalS +1
-            spadeProb= parseInt(totallS/6)
-            spadeOdd= parseInt(spadeProb-1)
+            // spadeProb= parseInt((totalS/6)*10)
+            // spadeOdd= parseInt(spadeProb-1)
+            spadeOdd = (13 - totalS) / 46;
+
         }
         
-        if( cutCards[1] =='d'){
+        if( cutCards[i][1] =='d'){
             totalD += 1
-            diamondProb= parseInt(totalD/6)
-            diamondOdd= parseInt(diamondProb-1)
+            // diamondProb= parseInt(totalD/6)
+            // diamondOdd= parseInt(diamondProb-1)
+            diamondOdd = (13 - totalD) / 46;
         }
         
-        if( cutCards[1] =='c'){
+        if( cutCards[i][1] =='c'){
             totalC += 1
-            clubProb= parseInt(totalC/6)
-            clubOdd= parseInt(clubProb-1)
-        }
-        console.log (totalC)
+            // clubProb= parseInt(totalC/6)
+            // clubOdd= parseInt(clubProb-1)
+            clubOdd = (13 - totalC) / 46;
         }
         
-        heartOdds.innertext = "Heart" + heartsProb + " / " + heartOdds + " Odds"
-        clubOdds.innertext = "Club" + clubsProb + " / " + heartOdds + " Odds"
-        heartOdds.innertext = "Heart" + heartsProb + " / " + heartOdds + " Odds"
-        heartOdds.innertext = "Heart" + heartsProb + " / " + heartOdds + " Odds" 
+    }
+       
 
- });
+        
 
 
+// Update the HTML elements with the calculated odds
+// heartOdds.innerHTML = "Heart Odds: " + (heartOdd * 100).toFixed(2) + "%";
+clubOdds.innerHTML = "Club Odds: " + (clubOdd * 100).toFixed(2) + "%";
+spadeOdds.innerHTML = "Spade Odds: " + (spadeOdd * 100).toFixed(2) + "%";
+diamondOdds.innerHTML = "Diamond Odds: " + (diamondOdd * 100).toFixed(2) + "%";
+        heartOdds.innerHTML = ("Heart " + heartsProb + " / " + heartOdd + " Odds")
+        // clubOdds.innerHTML = ("Club " + clubsProb + " / " + clubOdd + " Odds")
+        // diamondOdds.innerHTML = ("Diamond " + diamondProb + " / " + diamondOdd + " Odds")
+        // spadeOdds.innerHTML = ("Spades " + spadeProb + " / " + spadeOdd + " Odds") 
+        
+   },1500)
+});
 
 
-// calculate odds
+ 
+ 
 
 
-// counts suit
-// figure odds
-// for ()
-console.log(cutCards)
-git
+
+ heartbet.addEventListener("keyup", function(e){
+    if (e.key==="Enter"){
+        console.log(dollars)
+        if (heartbet.value> dollars){
+            console.log("sorry, you don't have enough money")
+        }else{
+            wager= heartbet.value
+        dollars-=wager
+        money.textContent= "$ " + dollars
+        heartbet.disabled = true 
+        betOn= "h"
+        }
+   
+        
+       
+    }
+})
+clubbet.addEventListener("keyup", function(e){
+    if (e.key==="Enter"){
+        console.log(dollars)
+        if (clubbet.value> dollars){
+            console.log("sorry, you don't have enough money")
+        }else{
+            wager= clubbet.value
+        dollars-=wager
+        money.textContent= "$ " + dollars
+        clubbet.disabled = true 
+        betOn= "c"
+        }
+   
+        
+       
+    }
+})
+spadebet.addEventListener("keyup", function(e){
+    if (e.key==="Enter"){
+        console.log(dollars)
+        if (spadebet.value> dollars){
+            console.log("sorry, you don't have enough money")
+        }else{
+            wager= spadebet.value
+        dollars-=wager
+        money.textContent= "$ " + dollars
+        spadebet.disabled = true 
+        betOn= "s"
+        }
+   
+        
+       
+    }
+})
+diamondbet.addEventListener("keyup", function(e){
+    if (e.key==="Enter"){
+        console.log(dollars)
+        if (diamondbet.value> dollars){
+            console.log("sorry, you don't have enough money")
+        }else{
+            wager= diamondbet.value
+        dollars-=wager
+        money.textContent= "$ " + dollars
+        diamondbet.disabled = true 
+        betOn= "d"
+        }
+   
+        
+       
+    }
+})
+
+
 
 
 
 //instuctions place your bet
-//bet subtracted from bank
+
 //instuctions to click card deck
-//finisher starts yo win or lose icon pops up 
-//calculate winnings and to bank
+
+
 // restart round
 // restart game
